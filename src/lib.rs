23c5a8c04b5b8e28acbfb3485cfdd0fe7f662da9
Alfred Tarng <atarng.A5T@gmail.com>
2024-11-01 11:17:47 -0700

    more wip

--- src/lib.rs
+++ src/lib.rs
@@ -9,16 +9,24 @@ use engage::gamedata::item::ItemData;
 #[unity::hook("App", "Unit", "ClassChange")]
 pub fn unit_classchange(this: &Unit, job: &JobData, item: &ItemData, _method_info : u64)
 {
-    let mut previous_level = this.fields.level;
+    let mut level_to_set_to = this.fields.level;
     if this.fields.level > 20 {
-        previous_level = this.fields.level - 20
+        level_to_set_to = this.fields.level - 20
+    }
+    else if this.get_job().get_max_level() == 20 && this.fields.level == 20 {
+        if this.get_job().is_high() {
+            level_to_set_to = 1
+        }
+        else {
+            level_to_set_to = 10
+        }
     }
 
     println!("running my class change");
     call_original!(this, job, item, _method_info);
 
     if item.fields.kind == 10 && item.fields.usetype == 24 {
-        this.set_level(previous_level.into());
+        this.set_level(level_to_set_to.into());
     }
 }
 
